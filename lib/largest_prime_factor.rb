4b70de395a38ce1c3780b7261b1f3cf73b26c57b
Kyle Tulod <tulod.kyle.m@gmail.com>
2017-02-03 15:38:01 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,23 @@
 # Enter your procedural solution here!
+def largest_prime_factor(input)
+  prime_factors(input).max
+end
+
+def prime_factors (natural_number)
+  prime_decomposition([natural_number]).flatten
+end
+
+def prime_decomposition (factors)
+  factors.collect do |factor|
+    if factor == 1 || factor.prime?
+       factor
+    else
+      divisor = 2
+      until factor % divisor == 0 do
+        divisor += 1
+      end
+      dividend = factor / divisor
+       prime_decomposition([divisor, dividend])
+    end
+  end
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,35 @@
 # Enter your object-oriented solution here!
+
+class LargestPrimeFactor
+
+    attr_accessor :number
+
+    def initialize (input)
+      self.number = largest_prime_factor(input)
+    end
+
+    private
+    def largest_prime_factor(input)
+      prime_factors(input).max
+    end
+
+    def prime_factors (natural_number)
+      prime_decomposition([natural_number]).flatten
+    end
+
+    def prime_decomposition (factors)
+      factors.collect do |factor|
+        if factor == 1 || factor.prime?
+           factor
+        else
+          divisor = 2
+          until factor % divisor == 0 do
+            divisor += 1
+          end
+          dividend = factor / divisor
+           prime_decomposition([divisor, dividend])
+        end
+      end
+    end
+
+end
